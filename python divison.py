4b43c3b170a92a925c2c4768b2be10baa910e0d2
user name <estheroluwaseye@hotmail.com>
2024-02-05 13:14:30 +0000

    updated

---
+++ python divison.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print (a//b) # print the input of A divided by B using the integer divison
+
+print (a/b) ## print the input of A divided by B using the integer divison
